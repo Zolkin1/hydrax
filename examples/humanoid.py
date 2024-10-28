0a185a2c8f639fd7afebfebfa32b8c485a383664
Zachary <zach.olkin@gmail.com>
2024-10-28 13:54:26 -0700

    added more algorithm options for the humanoid example

--- examples/humanoid.py
+++ examples/humanoid.py
@@ -1,8 +1,10 @@
+import sys
+
 import mujoco
 import numpy as np
 
 from hydrax import ROOT
-from hydrax.algs import PredictiveSampling
+from hydrax.algs import MPPI, PredictiveSampling, CEM
 from hydrax.mpc import run_interactive
 from hydrax.tasks.humanoid import Humanoid
 
@@ -14,7 +16,19 @@ Run an interactive simulation of the humanoid task.
 task = Humanoid()
 
 # Set up the controller
-ctrl = PredictiveSampling(task, num_samples=128, noise_level=0.2)
+# Set the controller based on command-line arguments
+if len(sys.argv) == 1 or sys.argv[1] == "ps":
+    print("Running predictive sampling")
+    ctrl = PredictiveSampling(task, num_samples=128, noise_level=0.2)
+elif sys.argv[1] == "mppi":
+    print("Running MPPI")
+    ctrl = MPPI(task, num_samples=128, noise_level=0.2, temperature=0.1)
+elif sys.argv[1] == "cem":
+    print("Running CEM")
+    ctrl = CEM(task, num_samples=128, num_elites=5, sigma_start=0.5, sigma_min=0.2)
+else:
+    print("Usage: python humanoid.py [ps|mppi]")
+    sys.exit(1)
 
 # Define the model used for simulation
 mj_model = mujoco.MjModel.from_xml_path(ROOT + "/models/g1/scene.xml")
